a76c0514204387e51983efa3da406807195dbc38
Danilo Mera <danilo@Danilos-Mac-mini.local>
2021-07-21 18:12:40 -0700

    UI enhancement, shows onloading and not found materials

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,38 @@
 import styles from '../styles/Home.module.css'
 import {useRouter} from "next/router"
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getById } from '../utils/dbMethods'
 import main from '../utils/dbConnection'
 
 
 export default function Home({data}) {
+  const router = useRouter()
   const initID = data ? data.materialID : "1000"
   const [materialId, SetMaterialId] = useState(initID)
-  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+
   const handleSubmit = e => {
     e.preventDefault()
+    setLoading(true)
     router.push(`/?id=${materialId}`)
+    //setTimeout(()=>{setLoading(false)}, 1000)
   }
+  useEffect(()=>{
+    setLoading(!(router.query.id == (data?.materialID)))
+    if(data ==null) setLoading(false)
+  },[router.query])
 
   return (
     <div className={styles.container}>
      <form onSubmit={handleSubmit} >
        <input  type="text" minLength="8" value={materialId} autoFocus={true} required  onChange={(e)=> SetMaterialId(e.target.value)}/>
      </form>
+    {/* if loading */}
+     {loading && (<div className={styles.loader}></div>)}
+     {/* if data is null */}
+     {((data == null) && (router.query.id))&& (<h3>{router.query.id} not found</h3>)}
      {/* if there is material, show pic + desc */}
-    {data && (
+    {(!loading && data) && (
       <div className={styles.infoGraph}>
         <img src={data.imageURL} alt={data.description}/>
         <h3>{data.description}</h3>
