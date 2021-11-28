765e95474040ba578bcda76725d1c614b06dee25
Danilo Mera <danilo@Danilos-Mac-mini.local>
2021-11-28 14:18:54 -0700

    empty hello method api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,8 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import {getById} from "../../utils/dbConnection"
 
 export default function handler(req, res) {
-  let obj = getById();
+  
 
-  res.status(200).json(obj)
+  res.status(200).json({hi:"hi"})
 }
